ec882ed47f1aa08a674116930042753b3b6eb1f5
kjanshair <kjanshair@outlook.com>
2019-03-06 11:15:02 +0500

    hello.py modified for ping.

--- src/hello/hello.py
+++ src/hello/hello.py
@@ -7,23 +7,12 @@ WORLD_ADDR = os.environ['WORLD_ADDR']
 WORLD_PORT = os.environ['WORLD_PORT']
 
 class handler(BaseHTTPRequestHandler):
-   def is_connected(self):
-     try:
-       host = socket.gethostbyname(WORLD_ADDR)
-       s = socket.create_connection((host, WORLD_PORT), 1)
-       return True
-     except:
-       pass
-     return False
-    
    def do_GET(self):
        self.send_response(200)
        self.send_header('Content-type','text/plain')
        self.end_headers()
 
-       result = self.is_connected()
-
-       if result:
+       if os.system("ping -c 1 -t 2 " + WORLD_ADDR) == 0:
            f = urllib.request.urlopen("http://" + WORLD_ADDR + ":" + WORLD_PORT.__str__() + "/api")
            self.wfile.write(b"Hello " + f.read())
        else:
